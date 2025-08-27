098a46c058aee9431873ce1f28144f2c1bd25aae
34lks66 <uranus667788@gmail.com>
2025-08-27 14:39:52 +0200

    add column grid

--- frontend/src/ToDo.js
+++ frontend/src/ToDo.js
@@ -4,6 +4,12 @@ export default function ToDo() {
   return (
     <div>
       <h1 className="title">Bienvenue dans le ToDo</h1>
-    </div>
+
+      <div className="parent">
+        <div className="div1">1</div>
+        <div className="div2">2</div>
+        <div className="div3">3</div>
+      </div>
+    </div>  
   );
 }
