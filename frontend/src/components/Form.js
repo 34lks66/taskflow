2a9b0ca068940404cb3f1c8134143dfd351613a8
Ezeloss <leo.guinvarch@gmail.com>
2025-08-30 00:44:45 +0200

    form create fixed

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,34 +1,51 @@
 import { useState } from "react";
 
-export default function Form( ) {
-  const [name, setName] = useState('');
-  const [state, setState] = useState('');
-  const [dateS, setDateS] = useState('');
-  const [dateE, setDateE] = useState('');
-  const [priority, setPriority] = useState('');
-  const [description, setDescription] = useState('');
+export default function Form( ) {  
+  
 
-  const onSubmit = (e) => {
+  const [TaskName, setName] = useState('');
+  const [TaskState, setState] = useState('A Faire');
+  const [TaskStartDate, setDateS] = useState('');
+  const [TaskEndDate, setDateE] = useState('');
+  const [TaskPriority, setPriority] = useState('Basse');
+  const [TaskDescritpion, setDescription] = useState('');
+
+  const onSubmit = async (e) => {
     e.preventDefault(); 
-    alert(`Submitted ${name} ${state} ${dateS} ${dateE} ${priority} ${description}`);
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+  TaskName: TaskName,
+  TaskState: TaskState,
+  TaskStartDate: TaskStartDate,
+  TaskEndDate: TaskEndDate,
+  TaskPriority: TaskPriority,
+  TaskDescription: TaskDescritpion
+})
+    };
+    fetch('http://localhost:5000/api/post', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
   };
 
   return (
     <form>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
-        <select value={state} onChange={(e) => setState(e.target.value)}>
+        <input value={TaskName} onChange={(e) => setName(e.target.value)} />
+        <select value={TaskState} onChange={(e) => setState(e.target.value)}>
             <option>A Faire</option>
             <option>En Cours</option>
             <option>Fini</option>
         </select>
-        <input type="date" value={dateS} onChange={(e) => setDateS(e.target.value)}/>
-        <input type="date" value={dateE} onChange={(e) => setDateE(e.target.value)}/>
-        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+        <input type="date" value={TaskStartDate} onChange={(e) => setDateS(e.target.value)}/>
+        <input type="date" value={TaskEndDate} onChange={(e) => setDateE(e.target.value)}/>
+        <select value={TaskPriority} onChange={(e) => setPriority(e.target.value)}>
             <option>Basse</option>
             <option>Moyenne</option>
             <option>Haute</option>
         </select>
-        <input value={description} onChange={(e) => setDescription(e.target.value)} />
+        <input value={TaskDescritpion} onChange={(e) => setDescription(e.target.value)} />
 
 
       <button onClick={onSubmit}>Submit</button>
